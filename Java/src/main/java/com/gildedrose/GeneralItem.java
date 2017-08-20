9a84599ec39697964ee58c15ac5f0154a9ed400c
Rachel <raquel.moreno.carmena@gmail.com>
2017-08-20 19:03:14 +0200

    Inline method

--- Java/src/main/java/com/gildedrose/GeneralItem.java
+++ Java/src/main/java/com/gildedrose/GeneralItem.java
@@ -10,16 +10,6 @@ public class GeneralItem extends Item {
         super(name, sellIn, quality);
     }
 
-    public static GeneralItem from(Item item) {
-        if (item.name.equals(SulfurasItem.SULFURAS_HAND_OF_RAGNAROS))
-            return new SulfurasItem(item);
-        if (item.name.equals(AgedBrieItem.AGED_BRIE))
-            return new AgedBrieItem(item);
-        if (item.name.equals(BackstageItem.BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT))
-            return new BackstageItem(item);
-        return new GeneralItem(item);
-    }
-
     public void updateItemQuality() {
         decreaseQuality();
         if (sellIn < 0) {
--- Java/src/main/java/com/gildedrose/ItemFactory.java
+++ Java/src/main/java/com/gildedrose/ItemFactory.java
@@ -2,6 +2,12 @@ package com.gildedrose;
 
 public class ItemFactory {
     public static GeneralItem from(Item item) {
-        return GeneralItem.from(item);
+        if (item.name.equals(SulfurasItem.SULFURAS_HAND_OF_RAGNAROS))
+            return new SulfurasItem(item);
+        if (item.name.equals(AgedBrieItem.AGED_BRIE))
+            return new AgedBrieItem(item);
+        if (item.name.equals(BackstageItem.BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT))
+            return new BackstageItem(item);
+        return new GeneralItem(item);
     }
 }
