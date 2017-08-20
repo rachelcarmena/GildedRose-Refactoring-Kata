7885cd82e4c7c4d69153e0ee580dc3b9e74665af
Rachel <raquel.moreno.carmena@gmail.com>
2017-08-20 18:46:19 +0200

    Move

--- Java/src/main/java/com/gildedrose/GeneralItem.java
+++ Java/src/main/java/com/gildedrose/GeneralItem.java
@@ -1,11 +1,63 @@
 package com.gildedrose;
 
 public class GeneralItem extends Item {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+
     public GeneralItem(Item item) {
         super(item.name, item.sellIn, item.quality);
     }
 
     public static GeneralItem from(Item item) {
         return new GeneralItem(item);
+    }
+
+    public void updateItemQuality() {
+        switch (name) {
+            case AGED_BRIE:
+                increaseQuality();
+                if (sellIn < 0) {
+                    increaseQuality();
+                }
+                break;
+            case BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT:
+                increaseQuality();
+                if (sellIn < 10) {
+                    increaseQuality();
+                }
+                if (sellIn < 5) {
+                    increaseQuality();
+                }
+                if (sellIn < 0) {
+                    quality = 0;
+                }
+                break;
+            case SULFURAS_HAND_OF_RAGNAROS:
+                break;
+            default:
+                decreaseQuality();
+                if (sellIn < 0) {
+                    decreaseQuality();
+                }
+        }
+    }
+
+    public void updateItemSellIn() {
+        if (!name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            sellIn -= 1;
+        }
+    }
+
+    public void decreaseQuality() {
+        if (quality > 0) {
+            quality -= 1;
+        }
+    }
+
+    public void increaseQuality() {
+        if (quality < 50) {
+            quality += 1;
+        }
     }
 }
--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,10 +2,6 @@ package com.gildedrose;
 
 class GildedRose {
 
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
-
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -15,57 +11,9 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             GeneralItem item = GeneralItem.from(items[i]);
-            updateItemSellIn(item);
-            updateItemQuality(item);
+            item.updateItemSellIn();
+            item.updateItemQuality();
             items[i] = item;
-        }
-    }
-
-    public void updateItemQuality(GeneralItem item) {
-        switch (item.name) {
-            case AGED_BRIE:
-                increaseQuality(item);
-                if (item.sellIn < 0) {
-                    increaseQuality(item);
-                }
-                break;
-            case BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT:
-                increaseQuality(item);
-                if (item.sellIn < 10) {
-                    increaseQuality(item);
-                }
-                if (item.sellIn < 5) {
-                    increaseQuality(item);
-                }
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                }
-                break;
-            case SULFURAS_HAND_OF_RAGNAROS:
-                break;
-            default:
-                decreaseQuality(item);
-                if (item.sellIn < 0) {
-                    decreaseQuality(item);
-                }
-        }
-    }
-
-    public void updateItemSellIn(GeneralItem item) {
-        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-            item.sellIn -= 1;
-        }
-    }
-
-    public void decreaseQuality(GeneralItem item) {
-        if (item.quality > 0) {
-            item.quality -= 1;
-        }
-    }
-
-    public void increaseQuality(GeneralItem item) {
-        if (item.quality < 50) {
-            item.quality += 1;
         }
     }
 }
